a708bda850247e68c2077c2ec56f645decf96f14
🦊 <gamilkarr@gmail.com>
2021-09-11 01:02:52 +0300

    fix models, pipfile

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from main import models
 
 admin.site.register(models.Animal)
-admin.site.register(models.AntiParasiteTreatment)
+admin.site.register(models.Treatment)
 admin.site.register(models.Vaccination)
--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Animal(models.Model):
@@ -8,15 +9,20 @@ class Animal(models.Model):
     age = models.PositiveIntegerField(verbose_name="Возраст")
     breed = models.CharField(max_length=30, verbose_name="Порода")
     species = models.CharField(max_length=30, verbose_name="Вид животного")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец")
 
     class Meta:
         verbose_name = "Животное"
         verbose_name_plural = "Животные"
+    
+    def __str__(self):
+        return self.name
 
 
 class Vaccination(models.Model):
     """Класс описывающий объект Вакцинация"""
 
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE, verbose_name="Животное")
     date = models.DateField(verbose_name="Дата прививки")
     vaccine = models.CharField(max_length=50, verbose_name="Вакцина")
 
@@ -24,10 +30,15 @@ class Vaccination(models.Model):
         verbose_name = "Вакцинация"
         verbose_name_plural = "Вакцинация"
 
+    def __str__(self):
+        return f"{self.date}"
+    
 
-class AntiParasiteTreatment(models.Model):
+
+class Treatment(models.Model):
     """Класс описывающий объект Обратока от паразитов"""
 
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE, verbose_name="Животное")
     parasite_species = models.CharField(max_length=30, verbose_name="Вид паразитов")
     date = models.DateField(verbose_name="Дата обработки")
     medication = models.CharField(max_length=50, verbose_name="Препарат")
@@ -36,3 +47,6 @@ class AntiParasiteTreatment(models.Model):
     class Meta:
         verbose_name = "Обработка от паразитов"
         verbose_name_plural = "Обработка от паразитов"
+
+    def __str__(self):
+        return f"{self.date}"
