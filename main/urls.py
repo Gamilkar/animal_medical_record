108fd71d64796323d3f9d7d850e52f531cb24dac
Gamilkar <gamilkarr@gmail.com>
2021-09-13 14:49:03 +0300

    edit views

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('<str:name>/', views.animal_card, name="animal_card"),
-    path('<str:name>/<str:procedures>', views.procedures_list, name="procedures_list"),
-
+    path("", views.index, name="index"),
+    path("add_animal/", views.add_animal, name="add_animal"),
+    path("<int:pk>/", views.animal_card, name="animal_card"),
+    path("<int:pk>/vaccinations", views.vaccinations, name="vaccinations"),
+    path("<int:pk>/treatments", views.treatments, name="treatments"),
+    path("<int:pk>/add_vaccination", views.add_vaccination, name="add_vaccination"),
+    path("<int:pk>/add_treatment", views.add_treatment, name="add_treatment"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,25 +2,60 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from main import models
 
+
 def index(request):
-    
-    animals_list = models.Animal.objects.filter(owner=User.objects.get(username=request.user.username))
+
+    animals_list = models.Animal.objects.filter(
+        owner=User.objects.get(username=request.user.username)
+    )
 
     context = {
-        'animals_list': animals_list
+        "animals_list": animals_list,
     }
     return render(request, "main/index.html", context)
 
-def animal_card(request, name):
-    context = {
-        'name': name
-    }
+
+def animal_card(request, pk):
+    animal = models.Animal.objects.get(id=pk)
+    context = {"animal": animal}
     return render(request, "main/animal_card.html", context)
 
-def procedures_list(request, name, procedures):
-    context = {
-        'name': name,
-        'procedures': procedures
-    }
-    return render(request, "main/procedures_list.html", context)
 
+def vaccinations(request, pk):
+    vaccinations = models.Vaccination.objects.filter(animal=pk)
+    context = {
+        "vaccinations": vaccinations,
+    }
+    return render(request, "main/vaccinations.html", context)
+
+
+def treatments(request, pk):
+    treatments = models.Treatment.objects.filter(animal=pk)
+    context = {
+        "treatments": treatments,
+    }
+    return render(request, "main/treatments.html", context)
+
+
+def add_vaccination(request, pk):
+
+    context = {
+        "status": "ok",
+    }
+    return render(request, "main/add_vaccination.html", context)
+
+
+def add_treatment(request, pk):
+
+    context = {
+        "status": "ok",
+    }
+    return render(request, "main/add_treatment.html", context)
+
+
+def add_animal(request):
+
+    context = {
+        "status": "ok",
+    }
+    return render(request, "main/add_animal.html", context)
